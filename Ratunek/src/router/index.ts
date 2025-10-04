6c7d4148fb59b01910230cf3b174c040abeddd3f
Kasia <kasiakadziolka@gmail.com>
2025-10-04 19:56:59 +0200

    Remove init files

--- Ratunek/src/router/index.ts
+++ Ratunek/src/router/index.ts
@@ -63,14 +63,6 @@ const router = createRouter({
       name: 'settings',
       component: SettingsView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
   ],
 })
 
