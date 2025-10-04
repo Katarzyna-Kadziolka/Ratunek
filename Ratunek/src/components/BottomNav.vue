485e6862f6062c33fdffb65fec0bc27f3f6fb1d5
Kasia <kasiakadziolka@gmail.com>
2025-10-04 19:44:42 +0200

    Remove button

--- Ratunek/src/components/BottomNav.vue
+++ Ratunek/src/components/BottomNav.vue
@@ -29,12 +29,6 @@ const router = useRouter()
       <span>Kryzys</span>
     </button>
 
-    <button class="nav-item">
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <path d="M8 6h13M8 12h13M8 18h13M3 6h.01M3 12h.01M3 18h.01" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-      <span>Lista</span>
-    </button>
   </nav>
 </template>
 
